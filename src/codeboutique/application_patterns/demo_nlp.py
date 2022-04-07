eb661270f4cb273ae9a27f878c15933b4cc2a6e0
Andy Miles <akmiles@icloud.com>
2022-04-06 20:43:25 -0700

    minor updates

---
+++ src/codeboutique/application_patterns/demo_nlp.py
@@ -0,0 +1,28 @@
+# https://www.nltk.org/
+
+
+import nltk
+import spacy
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
+
+
+
+text = """When we work with text, we can work with text units on different 
+scales: we can work at the level of the document itself, such as a newspaper 
+article; the paragraph, the sentence, or the word. Sentences are the main 
+unit of processing in many NLP tasks. In this section, I will show you how 
+to divide text into sentences.""".replace("\n", " ")
+
+sentences = tokenizer.tokenize(text)
+print(sentences)
+
+for sentence in sentences:
+    tokens = nltk.word_tokenize(sentence)
+    print(sentence)
+    print(tokens)
+    tagged = nltk.pos_tag(tokens)
+    print(tagged[0:6])
+    input()
